66240da865a2a94038fda9c33dc0f831d9e16008
Agni-AIT <agni.muhammad@akarinti.tech>
2024-07-22 13:03:11 +0700

    Initial Commit

---
+++ music_app/music_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  music_appApp.swift
+//  music_app
+//
+//  Created by Agni Muhammad on 22/07/24.
+//
+
+import SwiftUI
+
+@main
+struct music_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
